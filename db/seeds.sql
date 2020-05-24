007723e5d9717cfbb40aeced336dba2a82cb4a70
Matthew <mmacca473@gmail.com>
2020-05-24 14:23:14 +0930

    first push

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+INSERT INTO burgers (schnitzel_burger, false) VALUES (burger_name, devoured);
+
+INSERT INTO burgers (chicken_burger, false) VALUES (burger_name, devoured);
+
+INSERT INTO burgers (burglar, false) VALUES (burger_name, devoured);
+
+SELECT * FROM burgers;
